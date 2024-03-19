4675f059153e9aee24447f3c9cbe0b804b256b06
adnan <adnan.victor@student.moringaschool.com>
2024-03-19 11:33:54 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(currentBlock){
+ if(currentBlock>42){
+    return currentBlock - 42 ;
+ }
+ else {
+    return 42 - currentBlock
+ }
+}
+ function distanceFromHqInFeet(currentBlock){
+    return distanceFromHqInBlocks(currentBlock)*264 ;
+ }
+
+function distanceTravelledInFeet(start, end){
+    if (start>end){
+    return (start - end)*264 ;
+    }
+    else{
+    return (end - start)*264 ;
+    }
+}
+
+function calculatesFarePrice(start, end, fare){
+   const distance = distanceTravelledInFeet(start, end)
+   if (distance<=400){
+    return fare = 0 ;
+   }
+   else if (distance<=2000 && distance>400){
+    return fare = (distance-400) * 0.02 ;
+   }
+   else if (distance>2000 && distance<2500){
+    return 25 ;
+   }
+   else {
+    return 'cannot travel that far'
+   }
+}
